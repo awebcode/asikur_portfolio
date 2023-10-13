8804bcfcd98e52f5894c96e6506468e262799695
Asikur Rahman Developer <141431875+Md-Asikur-Rahman2@users.noreply.github.com>
2023-10-13 19:38:50 +0600

    Update auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -49,3 +49,33 @@ export const isAuthenticatedUser = async (req, res) => {
 //     next();
 //   };
 // };
+
+//************************************************************
+
+///if i use it don't show api stolled request err in next js project
+// In your auth middleware file (e.g., authMiddleware.js)
+// import jwt from "jsonwebtoken";
+
+// export const authMiddleware = (req, res, next) => {
+//   return new Promise((resolve, reject) => {
+//     // Get the token from the request headers or cookies
+//      console.log(req.cookies.nexttoken);
+//     const token = req.headers.authorization || req.cookies.nexttoken;
+
+//     if (!token) {
+//       res.status(401).json({ message: "Unauthorized" });
+//       reject();
+//     }
+
+//     try {
+//       // Verify the token
+//       const decoded = jwt.verify(token, process.env.SECRET); // Change with your secret key
+//       req.user = decoded;
+//       resolve();
+//     } catch (error) {
+//       res.status(401).json({ message: "Invalid token" });
+//       reject();
+//     }
+//   });
+// };
+
