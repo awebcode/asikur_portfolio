f38fe2e3d0d1ba69c85990a7b83be9f4d577b3fe
Md-Asikur <asikurrahaman997@gamil.com>
2023-10-16 07:51:49 +0600

    xs

--- components/Home.js
+++ components/Home.js
@@ -64,7 +64,7 @@ const Home = () => {
           span4="with 4 years of experience."
           span5="Client satisfaction is my first priority, and let me claim that we are best at it. I believe in open communication, transparency, and exceeding expectations."
           btntext="Hire Me"
-          btnloc="/blog/Home"
+          btnloc="https://www.facebook.com/md.asikur.9047506/"
           img="/asik3.jpg"
         />
         {/* <MoveRocket/> */}
